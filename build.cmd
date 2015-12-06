54f58e5dadd1ec6bcda691f10b3a39ffddb6d57d
Jared Klopper <spuzzy2@hotmail.com>
2015-12-06 23:06:27 +1300

    Add appveyor build configuration and windows build batch file

---
+++ build.cmd
@@ -0,0 +1,18 @@
+where dnvm
+
+if %ERRORLEVEL% neq 0 (
+    @powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
+    set PATH=!PATH!;!USERPROFILE!\.dnx\bin
+    set DNX_HOME=!USERPROFILE!\.dnx
+    goto install
+)
+
+:install
+call dnvm update-self
+call dnvm install 1.0.0-rc1-update1
+call dnvm use 1.0.0-rc1-update1
+
+pushd Teloqui
+call dnu restore
+call dnu build
+popd
