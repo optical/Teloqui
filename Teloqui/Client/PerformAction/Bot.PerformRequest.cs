2f6c556818a9248935a774cc054daadf4aeec11a
Fausto Payano <faustopayano@gmail.com>
2015-12-17 00:07:10 -0500

    Re-arranged SendMessageAsync to Partial classes
    Modified SendMessageAsync and placed it in partial classes with
    overloaded methods. Will review and change if needed.
    
    Perform requests are in the PerformAction folder, SendMessage partial
    classes are in the SendMessage folder.

---
+++ Teloqui/Client/PerformAction/Bot.PerformRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Teloqui.Data;
+
+namespace Teloqui.Client.SendMessage
+{
+    internal partial class Bot
+    {
+        private readonly JsonSerializer _serializer;
+
+        private async Task<ApiResponse<T>> PerformRequest<T>(Func<Task<HttpResponseMessage>> messageFactory) {
+            HttpResponseMessage message = await messageFactory();
+            message.EnsureSuccessStatusCode();
+
+            using (Stream stream = await message.Content.ReadAsStreamAsync()) {
+                using (StreamReader reader = new StreamReader(stream)) {
+                    using (JsonReader jsonReader = new JsonTextReader(reader)) {
+                        return _serializer.Deserialize<ApiResponse<T>>(jsonReader);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Teloqui/Client/SendMessage/Bot.SendMessageAsync.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Teloqui.Data;
+
+namespace Teloqui.Client.SendMessage
+{
+    using MessageResponse = Task<ApiResponse<Message>>;
+    using ParameterList = Dictionary<string, string>;
+
+    enum ReplyMarkup {
+        None,
+        ReplyKeyboardMarkup,
+        ReplyKeyboardHide,
+        ForceReply
+    }
+
+    internal partial class Bot {
+
+       /* public MessageResponse SendMessageAsync(
+            string destinationId,
+            string message,
+            string messageModifier,
+            bool? disableWebPagePreview,
+            int? replyToMessageId,
+            bool isMarkDown = false,
+            ReplyMarkup replyToMarkup = null,
+            CancellationToken cancellationToken = default(CancellationToken)) {
+
+            var parameters = new ParameterList() {
+                ["text"] = message
+            };
+            if (isMarkDown) {
+                parameters["parse_mode"] = "Markdown";
+            }
+
+            return SendMessageInternal("sendMessage", destinationId, parameters, disableWebPagePreview, replyToMessageId,
+                replyToMarkup, cancellationToken);
+        }
+        */
+
+        public MessageResponse SendMessageAsync(
+            string destinationId,
+            string message,
+            string messageModifier) {
+
+            return SendMessageAsync(destinationId, message, messageModifier, null, null, false, ReplyMarkup.None, null);
+        }
+
+        public MessageResponse SendMessageAsync(
+            string destinationId,
+            string message,
+            bool? disableWebPagePreview) {
+
+            return SendMessageAsync(destinationId, message, string.Empty, disableWebPagePreview, null, false, ReplyMarkup.None, null);
+        }
+
+        public MessageResponse SendMessageAsync(
+           string destinationId,
+           string message,
+           int? replyToMessageId) {
+
+            return SendMessageAsync(destinationId, message, string.Empty, null, replyToMessageId, false, ReplyMarkup.None, null);
+        }
+
+        public MessageResponse SendMessageAsync(
+           string destinationId,
+           string message,
+           bool isMarkdown) {
+
+            return SendMessageAsync(destinationId, message, string.Empty, null, null, isMarkdown, ReplyMarkup.None, null);
+        }
+
+        public MessageResponse SendMessageAsync(
+           string destinationId,
+           string message,
+           ReplyMarkup replyToMarkup) {
+
+            return SendMessageAsync(destinationId, message, string.Empty, null, null, false, replyToMarkup, null);
+        }
+
+        public MessageResponse SendMessageAsync(
+            string destinationId,
+            string message,
+            string messageModifier,
+            int? replyToMessageId) {
+
+            return SendMessageAsync(destinationId, message, messageModifier, null, replyToMessageId, false, ReplyMarkup.None, null);
+        }
+
+        public MessageResponse SendMessageAsync(
+            string destinationId,
+            string message,
+            string messageModifier,
+            bool? disableWebPagePreview,
+            int? replytoMessageId,
+            bool isMarkdown,
+            ReplyMarkup replyToMarkup,
+            CancellationToken? cancellationToken) {
+            var parameters = new ParameterList() {
+                ["text"] = message
+            };
+            if (isMarkdown) {
+                parameters["parse_mode"] = "Markdown";
+            }
+
+
+            //Possible System.InvalidOperationException - Please review the use of Cancellation Token here as a non Optional parameter.
+            return SendMessageInternal("sendMessage", destinationId, parameters, disableWebPagePreview, replytoMessageId,
+                replyToMarkup, cancellationToken.Value);
+
+        }
+
+
+
+
+    }
+}
